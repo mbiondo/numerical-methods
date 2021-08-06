db2b596c86e1e3eb65d717237575b97ba5d84708
stn <smrcouto88@gmail.com>
2021-08-06 03:41:33 -0300

    anchorLinks y presentacion

--- pages/slides.js
+++ pages/slides.js
@@ -5,7 +5,9 @@ import { BiRadioCircle, BiRadioCircleMarked } from 'react-icons/bi'
 import makeCarousel from 'react-reveal/makeCarousel'
 import Fade from 'react-reveal/Fade'
 import Link from 'next/link'
-import Slide from 'react-reveal/Slide';
+import Slide from 'react-reveal/Slide'
+import Image from 'next/image'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Intro = () => {
   const Carousel = makeCarousel(CarouselUI)
@@ -27,55 +29,55 @@ const Intro = () => {
         />
       </Head>
       <div className="w-full pb-24 bg-gray-100">
-        <div className="relative flex flex-col items-center justify-center max-w-6xl py-64 mx-auto overflow-hidden text-center text-gray-900 bg-gray-100 ">
-          <div className="absolute inset-0 z-0">
-            <Carousel
-              defaultWait={4000}
-              maxTurns={99} /*wait for 1000 milliseconds*/
-            >
-              <Slide right>
-                <div className="flex flex-col items-center justify-center h-full py-24">
-                  <Fade>
-                    <h2 className="text-6xl text-center ">Métodos numéricos</h2>
-                  </Fade>
-                  <Fade delay={100}>
-                    <h3 className="mt-2 font-sans text-3xl italic text-center">
-                      Resolución numérica de ecuaciones
-                    </h3>
-                  </Fade>
-                  <Fade delay={120}>
-                    <Link href="/">
-                      <a className="inline-block p-2 px-4 mt-4 font-bold text-pink-100 duration-100 bg-pink-700 rounded-md cursor-pointer hover:bg-pink-800">
-                        Introducción
-                      </a>
-                    </Link>
-                  </Fade>
-                </div>
-              </Slide>
-              <Slide right>
-                <div className="flex flex-col items-center justify-center h-full py-24">
-
-                  <Link href="/methods/bisection">
-                    <a className="inline-block p-2 px-4 mt-4 font-bold text-pink-100 duration-100 bg-pink-700 rounded-md cursor-pointer hover:bg-pink-800">
-                    Métodos
-                    </a>
-                  </Link>
-                </div>
-              </Slide>
-              <Slide right>
-                <div className="flex flex-col items-center justify-center h-full py-24">
-                  <Link href="/playground">
-                    <a className="inline-block p-2 px-4 mt-4 font-bold text-pink-100 duration-100 bg-pink-700 rounded-md cursor-pointer hover:bg-pink-800">
-                      Playground
-                    </a>
-                  </Link>
-                </div>
-              </Slide>
-            </Carousel>
-          </div>
+        <div className="fixed z-50 flex flex-col p-2 space-y-2 text-gray-500 bg-white shadow left-4 top-24 ">
+          <AnchorLink
+            className="w-full px-3 py-2 border-l-2 focus:text-pink-800 focus:font-bold focus:border-pink-800 focus:bg-pink-50"
+            href="#introduccion"
+          >
+            Introducción
+          </AnchorLink>
+          <AnchorLink
+            className="w-full px-3 py-2 border-l-2 focus:text-pink-800 focus:font-bold focus:border-pink-800 focus:bg-pink-50"
+            href="#metodo-de-bisección"
+          >
+            Método de Bisección.
+          </AnchorLink>
+          <AnchorLink
+            className="w-full px-3 py-2 border-l-2 focus:text-pink-800 focus:font-bold focus:border-pink-800 focus:bg-pink-50"
+            href="#newton"
+          >
+            Método de Newton
+          </AnchorLink>
+          <AnchorLink
+            className="w-full px-3 py-2 border-l-2 focus:text-pink-800 focus:font-bold focus:border-pink-800 focus:bg-pink-50"
+            href="#secante"
+          >
+            Método de la secante
+          </AnchorLink>
+          <AnchorLink
+            className="w-full px-3 py-2 border-l-2 focus:text-pink-800 focus:font-bold focus:border-pink-800 focus:bg-pink-50"
+            href="#regla-falsa"
+          >
+            Método de la regla Falsa
+          </AnchorLink>
+          <AnchorLink
+            className="w-full px-3 py-2 border-l-2 focus:text-pink-800 focus:font-bold focus:border-pink-800 focus:bg-pink-50"
+            href="#punto-fijo"
+          >
+            Método del punto fijo
+          </AnchorLink>
+          <AnchorLink
+            className="w-full px-3 py-2 border-l-2 focus:text-pink-800 focus:font-bold focus:border-pink-800 focus:bg-pink-50"
+            href="#comparativa"
+          >
+            Comparativa
+          </AnchorLink>
         </div>
         <section className="max-w-5xl mx-auto mt-6 bg-white shadow">
-          <article className="px-3 py-24 mx-auto font-serif prose lg:prose-xl">
+          <article
+            id="introduccion"
+            className="px-3 py-24 mx-auto font-serif prose lg:prose-xl"
+          >
             <h3>Introducción</h3>
             <p>
               Uno de los problemas que más se presenta en matemáticas es el de
@@ -106,53 +108,186 @@ const Intro = () => {
               primer grado (interpolación lineal) y después aproximar la
               solución mediante alguna de las raíces del polinomio.
             </p>
-            <p>
-              El algoritmo más simple de búsqueda de raíces es el método de
-              bisección. Requiere un intervalo inicial que contenga alguna raíz
-              de la ecuación (de forma que la función tome en los extremos del
-              mismo valores de distinto signo; véase el teorema de Bolzano).
-              Dicho intervalo inicial se va dividiendo sucesivamente por la
-              mitad (se bisecta) tomándose el intervalo que contiene a la raíz.
-              A pesar de ser un método que siempre converge a una solución,
-              converge muy lentamente.
-            </p>
-            <p>
-              El método de Newton asume que la función f sea continuamente
-              derivable y que se conoce la derivada de la función. Este método
-              puede no converger si se comienza con un valor muy alejado de la
-              raíz. Sin embargo, si converge, lo hace mucho más rápido que el
-              método de bisección (usualmente, de manera cuadrática), por eso el
-              número de dígitos correctos se duplica en cada iteración. El
-              método de Newton también es útil porque se generaliza para
-              problemas de dimensiones más altas.
-            </p>
-            <p>
-              Reemplazando la derivada del método de Newton por un cociente
-              incremental, obtenemos el método de la secante. Este método no
-              requiere el cálculo (ni la existencia) de la derivada, pero el
-              precio que se debe pagar es un orden de convergencia más bajo
-              (aproximadamente 1.6).
-            </p>
-            <p>
-              El método de la regla falsa (o regula falsi) es un método que
-              combina lo mejor del método de bisección y del método de la
-              secante. El método corta el intervalo en dos partes como en el
-              método de bisección, pero a diferencia de éste, lo corta por el
-              valor obtenido aplicando el método de la secante a los extremos
-              del intervalo, no siendo generalmente las partes iguales. El
-              método converge siempre a una raíz de la ecuación, generalmente de
-              forma más rápida que el método de bisección pero más lenta que el
-              método de la secante.
-            </p>
-            <p>
-              Finalmente, hay una familia de métodos conocidos como métodos de
-              punto fijo. Estos métodos se basan en obtener a partir de la
-              ecuación f(x) = 0 una ecuación equivalente de la forma g(x) = x
-              cuya solución se convierta en un punto fijo de g e iterando a
-              partir de un valor inicial hasta que se alcance.
-            </p>
           </article>
         </section>
+        <div
+          className="relative flex items-center justify-center min-h-screen bg-pink-800"
+          id="metodo-de-bisección"
+        >
+          <div className="grid grid-cols-2 gap-2 mx-auto max-w-7xl ">
+            <Fade delay={400}>
+              <Image
+                layout="responsive"
+                src="/images/bisection_1.png"
+                alt="Ejemplo grafico"
+                width={1527}
+                height={1079}
+              />
+            </Fade>
+            <Fade>
+              <div className="flex flex-col items-center justify-center p-6 text-left text-white">
+                <h3 className="w-full pb-6 text-4xl font-bold text-left text-white">
+                  Método de bisección.
+                </h3>
+                <p className="text-2xl text-white ">
+                  El algoritmo más simple de búsqueda de raíces es el método de
+                  bisección. Requiere un intervalo inicial que contenga alguna
+                  raíz de la ecuación (de forma que la función tome en los
+                  extremos del mismo valores de distinto signo; véase el teorema
+                  de Bolzano). Dicho intervalo inicial se va dividiendo
+                  sucesivamente por la mitad (se bisecta) tomándose el intervalo
+                  que contiene a la raíz. A pesar de ser un método que siempre
+                  converge a una solución, converge muy lentamente.
+                </p>
+                <div className="flex justify-start w-full">
+                  <Link href="/methods/bisection">
+                    <a className="inline-block p-2 px-4 mt-4 font-bold text-pink-900 duration-100 bg-white rounded-md cursor-pointer hover:bg-pink-300">
+                      Ver detalle
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </Fade>
+          </div>
+        </div>
+        <div
+          className="relative flex items-center justify-center min-h-screen bg-pink-800"
+          id="newton"
+        >
+          <div className="grid grid-cols-2 gap-2 mx-auto max-w-7xl ">
+            <Fade>
+              <div className="flex flex-col items-center justify-center p-6 text-left text-white">
+                <h3 className="w-full pb-6 text-4xl font-bold text-left text-white">
+                  Método de Newton.
+                </h3>
+                <p className="text-2xl text-white ">
+                  El método de Newton asume que la función f sea continuamente
+                  derivable y que se conoce la derivada de la función. Este
+                  método puede no converger si se comienza con un valor muy
+                  alejado de la raíz. Sin embargo, si converge, lo hace mucho
+                  más rápido que el método de bisección (usualmente, de manera
+                  cuadrática), por eso el número de dígitos correctos se duplica
+                  en cada iteración. El método de Newton también es útil porque
+                  se generaliza para problemas de dimensiones más altas.
+                </p>
+              </div>
+            </Fade>
+            <Fade delay={400}>
+              <Image
+                layout="responsive"
+                src="/images/bisection_1.png"
+                alt="Ejemplo grafico"
+                width={1527}
+                height={1079}
+              />
+            </Fade>
+          </div>
+        </div>
+        <div
+          className="relative flex items-center justify-center min-h-screen bg-pink-800"
+          id="secante"
+        >
+          <div className="grid grid-cols-2 gap-2 mx-auto max-w-7xl ">
+            <Fade delay={400}>
+              <Image
+                layout="responsive"
+                src="/images/bisection_1.png"
+                alt="Ejemplo grafico"
+                width={1527}
+                height={1079}
+              />
+            </Fade>
+            <Fade>
+              <div className="flex flex-col items-center justify-center p-6 text-left text-white">
+                <h3 className="w-full pb-6 text-4xl font-bold text-left text-white">
+                  Método de la Secante.
+                </h3>
+                <p className="text-2xl text-white ">
+                  Reemplazando la derivada del método de Newton por un cociente
+                  incremental, obtenemos el método de la secante. Este método no
+                  requiere el cálculo (ni la existencia) de la derivada, pero el
+                  precio que se debe pagar es un orden de convergencia más bajo
+                  (aproximadamente 1.6).
+                </p>
+              </div>
+            </Fade>
+          </div>
+        </div>
+
+        <div
+          className="relative flex items-center justify-center min-h-screen bg-pink-800"
+          id="regla-falsa"
+        >
+          <div className="grid grid-cols-2 gap-2 mx-auto max-w-7xl ">
+            <Fade delay={400}>
+              <Image
+                layout="responsive"
+                src="/images/bisection_1.png"
+                alt="Ejemplo grafico"
+                width={1527}
+                height={1079}
+              />
+            </Fade>
+            <Fade>
+              <div className="flex flex-col items-center justify-center p-6 text-left text-white">
+                <h3 className="w-full pb-6 text-4xl font-bold text-left text-white">
+                  Método de la regla falsa
+                </h3>
+                <p className="text-2xl text-white ">
+                  El método de la regla falsa (o regula falsi) es un método que
+                  combina lo mejor del método de bisección y del método de la
+                  secante. El método corta el intervalo en dos partes como en el
+                  método de bisección, pero a diferencia de éste, lo corta por
+                  el valor obtenido aplicando el método de la secante a los
+                  extremos del intervalo, no siendo generalmente las partes
+                  iguales. El método converge siempre a una raíz de la ecuación,
+                  generalmente de forma más rápida que el método de bisección
+                  pero más lenta que el método de la secante.
+                </p>
+              </div>
+            </Fade>
+          </div>
+        </div>
+
+        <div
+          className="relative flex items-center justify-center min-h-screen bg-pink-800"
+          id="punto-fijo"
+        >
+          <div className="grid grid-cols-2 gap-2 mx-auto max-w-7xl ">
+            <Fade>
+              <div className="flex flex-col items-center justify-center p-6 text-left text-white">
+                <h3 className="w-full pb-6 text-4xl font-bold text-left text-white">
+                  Método del punto fijo
+                </h3>
+                <p className="text-2xl text-white ">
+                  Finalmente, hay una familia de métodos conocidos como métodos
+                  de punto fijo. Estos métodos se basan en obtener a partir de
+                  la ecuación f(x) = 0 una ecuación equivalente de la forma g(x)
+                  = x cuya solución se convierta en un punto fijo de g e
+                  iterando a partir de un valor inicial hasta que se alcance.
+                </p>
+              </div>
+            </Fade>
+            <Fade delay={400}>
+              <Image
+                layout="responsive"
+                src="/images/bisection_1.png"
+                alt="Ejemplo grafico"
+                width={1527}
+                height={1079}
+              />
+            </Fade>
+          </div>
+        </div>
+
+        <div
+          className="relative flex items-center justify-center min-h-screen bg-pink-800"
+          id="comparativa"
+        >
+          <div className="relative flex flex-col items-center justify-center w-full max-w-6xl mx-auto overflow-hidden text-center text-gray-900 bg-gray-100 ">
+            <h2 className="py-6 text-3xl text-center">Comparativa</h2>
+          </div>
+        </div>
       </div>
     </>
   )
@@ -160,7 +295,7 @@ const Intro = () => {
 export default Intro
 
 const CarouselUI = ({ position, total, handleClick, children }) => (
-  <div className="relative w-full h-full mb-0 overflow-hidden border border-pink-100 shadow-lg">
+  <div className="relative w-full overflow-hidden border border-pink-100 shadow-lg h-80">
     <div>
       <div
         className="absolute z-50 items-center justify-center hidden w-12 h-12 -mt-6 text-5xl text-center text-pink-900 duration-200 transform cursor-pointer hover:scale-105 hover:translate-x-1 left-2 hover:text-gray-600 md:flex top-1/2"
